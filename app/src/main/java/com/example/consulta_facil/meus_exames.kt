78f53c77976d2d9907605f86c3aa150e18ae50ef
git config --global user.name Fernando <fernandoeqc@live.com>
2024-10-12 23:26:54 -0300

    - Create main screen - Create minhas consultas - Create meus exames

--- app/src/main/java/com/example/consulta_facil/meus_exames.kt
+++ app/src/main/java/com/example/consulta_facil/meus_exames.kt
@@ -6,26 +6,15 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-import android.widget.Button
-import android.content.Intent
-import android.widget.ImageButton
-
-
-class MainActivity : AppCompatActivity() {
+class meus_exames : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_meus_exames)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        val buttonClick = findViewById<ImageButton>(R.id.button_meus_exames)
-        buttonClick.setOnClickListener {
-            val intent = Intent(this, minhas_consultas::class.java)
-            startActivity(intent)
         }
     }
 }
