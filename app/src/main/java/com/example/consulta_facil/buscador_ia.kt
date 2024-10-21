900785cbbe7ffadf90de56a030d1b1c60a8d5cb0
UltiMaia <plucasmaiadelima@gmail.com>
2024-10-21 14:31:24 -0300

    add: buscador de IA

---
+++ app/src/main/java/com/example/consulta_facil/buscador_ia.kt
@@ -0,0 +1,14 @@
+package com.example.consulta_facil
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class buscador_ia : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_buscador_ia)
+
+    }
+}
